6063ad75478740c500107ba4dbb1de6b12aa5f3f
Ansh Agnihotri <167695711+anshedu@users.noreply.github.com>
2025-06-18 09:12:20 +0530

    Update Matrixx.sh

--- Matrixx.sh
+++ Matrixx.sh
@@ -12,11 +12,11 @@ git clone https://github.com/anshedu/android_kernel_oneplus_sm6375 -b lineage-22
 git clone https://github.com/anshedu/android_hardware_oplus -b lineage-22.2 hardware/oplus && \
 # Vanilla Build
 . build/envsetup.sh && \
-lineage_larry-user && make installclean && mka bacon; \
+breakfast larry user && make installclean && mka bacon; \
 rm -rf out/target/product/vanilla && rm -rf out/target/product/gapps; \
 cd out/target/product && mv larry vanilla && cd ../../..; \
 # Gapps Build
 cd device/oneplus/larry && rm lineage_larry.mk && mv gapps.txt lineage_larry.mk && cd ../../..; \
 . build/envsetup.sh; \
-lineage_larry-user && make installclean && mka bacon; \
+breakfast larry user && make installclean && mka bacon; \
 cd out/target/product && mv larry gapps && cd ../../..; \
