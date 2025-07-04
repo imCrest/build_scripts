fafb6ab01542cf55ffef2d5d8b4e74b32974565c
Ansh Agnihotri <167695711+anshedu@users.noreply.github.com>
2025-07-04 18:23:29 +0530

    Update EvolutionX.sh

--- EvolutionX.sh
+++ EvolutionX.sh
@@ -4,6 +4,23 @@ rm -rf .repo/local_manifests; \
 rm -rf {device,vendor,kernel,hardware}/oneplus && \
 repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b vic --git-lfs -g default,-mips,-darwin,-notdefault && \
 /opt/crave/resync.sh && \
+# ==== Fix for Trusty Soong Error (dummy module define) ====
+
+mkdir -p trusty/vendor/google/aosp/scripts/dummy
+
+cat > trusty/vendor/google/aosp/scripts/Android.bp << 'EOF'
+bootstrap_go_package {
+    name: "trusty_dirgroup_prebuilts_clang_host_linux-x86",
+    pkgPath: "trusty/vendor/google/aosp/scripts/dummy",
+    deps: [],
+    srcs: ["dummy.go"],
+}
+EOF
+
+echo "package dummy" > trusty/vendor/google/aosp/scripts/dummy/dummy.go
+
+# ==== End of Trusty Fix ====
+
 git clone https://github.com/anshedu/android_device_oneplus_larry -b lineage-22.2 device/oneplus/larry && \
 git clone https://github.com/anshedu/android_device_oneplus_sm6375-common -b lineage-22.2 device/oneplus/sm6375-common && \
 git clone https://github.com/anshedu/proprietary_vendor_oneplus_larry -b lineage-22.2 vendor/oneplus/larry && \
