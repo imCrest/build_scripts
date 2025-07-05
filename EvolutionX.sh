d67aa66ff059b319ab61ca5a04383f71a14dcfe7
Ansh Agnihotri <167695711+anshedu@users.noreply.github.com>
2025-07-05 21:19:46 +0530

    Update EvolutionX.sh

--- EvolutionX.sh
+++ EvolutionX.sh
@@ -2,6 +2,7 @@
 
 rm -rf .repo/local_manifests; \
 rm -rf {device,vendor,kernel,hardware}/oneplus && \
+rm -rf prebuilts/clang/host/linux-x86 && \
 repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b vic --git-lfs -g default,-mips,-darwin,-notdefault && \
 /opt/crave/resync.sh && \
 # ==== Fix for Trusty Soong Error (dummy module define) ====
