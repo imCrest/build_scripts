582c6d594a15ec16d460a029e0bd5e32ce97d272
Ansh Agnihotri <167695711+anshedu@users.noreply.github.com>
2025-07-07 21:25:15 +0530

    Update InfinityX.sh

--- InfinityX.sh
+++ InfinityX.sh
@@ -1,42 +1,23 @@
-#!/bin/bash
+#! /bin/bash
 
-# Clean up existing manifest and device sources
-rm -rf .repo/local_manifests
-rm -rf {device,vendor,kernel,hardware}/oneplus
-
-# Initialize repo
-repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 15 -g default,-mips,-darwin,-notdefault
-
-# Resync sources
-/opt/crave/resync.sh
-
-# Clone required device, vendor, kernel, hardware repos
-git clone https://github.com/anshedu/android_device_oneplus_larry -b InfinityX device/oneplus/larry
-git clone https://github.com/anshedu/android_device_oneplus_sm6375-common -b InfinityX device/oneplus/sm6375-common
-git clone https://github.com/anshedu/proprietary_vendor_oneplus_larry -b lineage-22.2 vendor/oneplus/larry
-git clone https://github.com/anshedu/proprietary_vendor_oneplus_sm6375-common -b lineage-22.2 vendor/oneplus/sm6375-common
-git clone https://github.com/anshedu/android_kernel_oneplus_sm6375 -b lineage-22.2 kernel/oneplus/sm6375
-git clone https://github.com/anshedu/hardware_oplus -b fifteen hardware/oplus
-
-# --- Vanilla Build ---
-. build/envsetup.sh
-lunch larry-user
-make installclean
-mka bacon
-cd out/target/product
-mv larry vanilla
-cd ../../..
-
-# --- GApps Build ---
-cd device/oneplus/larry
-rm infinity_larry.mk
-mv gapps.txt infinity_larry.mk
-cd ../../..
-
-. build/envsetup.sh
-lunch larry-user
-make installclean
-mka bacon
-cd out/target/product
-mv larry gapps
-cd ../../..
+rm -rf .repo/local_manifests; \
+rm -rf {device,vendor,kernel,hardware}/oneplus && \
+rm -rf prebuilts/clang/host/linux-x86 \
+repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 15 -g default,-mips,-darwin,-notdefault && \
+/opt/crave/resync.sh && \
+git clone https://github.com/anshedu/android_device_oneplus_larry -b lineage-22.2 device/oneplus/larry && \
+git clone https://github.com/anshedu/android_device_oneplus_sm6375-common -b lineage-22.2 device/oneplus/sm6375-common && \
+git clone https://github.com/anshedu/proprietary_vendor_oneplus_larry -b lineage-22.2 vendor/oneplus/larry && \
+git clone https://github.com/anshedu/proprietary_vendor_oneplus_sm6375-common -b lineage-22.2 vendor/oneplus/sm6375-common && \
+git clone https://github.com/anshedu/android_kernel_oneplus_sm6375 -b lineage-22.2 kernel/oneplus/sm6375 && \
+git clone https://github.com/anshedu/android_hardware_oplus -b lineage-22.2 hardware/oplus && \
+# Vanilla Build
+. build/envsetup.sh && \
+lunch larry user && make installclean && mka bacon; \
+rm -rf out/target/product/vanilla && rm -rf out/target/product/gapps; \
+cd out/target/product && mv larry vanilla && cd ../../..; \
+# Gapps Build
+cd device/oneplus/larry && rm infinity_larry.mk && mv gapps.txt lineage_larry.mk && cd ../../..; \
+. build/envsetup.sh; \
+lunch larry user && make installclean && mka bacon; \
+cd out/target/product && mv larry gapps && cd ../../..; \
