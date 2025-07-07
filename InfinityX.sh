cd39b0152ea4ecc097c61363ee52deee83c87ef0
Ansh Agnihotri <167695711+anshedu@users.noreply.github.com>
2025-07-07 21:27:07 +0530

    Update InfinityX.sh

--- InfinityX.sh
+++ InfinityX.sh
@@ -5,11 +5,11 @@ rm -rf {device,vendor,kernel,hardware}/oneplus && \
 rm -rf prebuilts/clang/host/linux-x86 \
 repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 15 -g default,-mips,-darwin,-notdefault && \
 /opt/crave/resync.sh && \
-git clone https://github.com/anshedu/android_device_oneplus_larry -b lineage-22.2 device/oneplus/larry && \
-git clone https://github.com/anshedu/android_device_oneplus_sm6375-common -b lineage-22.2 device/oneplus/sm6375-common && \
-git clone https://github.com/anshedu/proprietary_vendor_oneplus_larry -b lineage-22.2 vendor/oneplus/larry && \
-git clone https://github.com/anshedu/proprietary_vendor_oneplus_sm6375-common -b lineage-22.2 vendor/oneplus/sm6375-common && \
-git clone https://github.com/anshedu/android_kernel_oneplus_sm6375 -b lineage-22.2 kernel/oneplus/sm6375 && \
+git clone https://github.com/anshedu/android_device_oneplus_larry -b AOSP-15 device/oneplus/larry && \
+git clone https://github.com/anshedu/android_device_oneplus_sm6375-common -b AOSP-15 device/oneplus/sm6375-common && \
+git clone https://github.com/anshedu/proprietary_vendor_oneplus_larry -b AOSP-15 vendor/oneplus/larry && \
+git clone https://github.com/anshedu/proprietary_vendor_oneplus_sm6375-common -b AOSP-15 vendor/oneplus/sm6375-common && \
+git clone https://github.com/anshedu/android_kernel_oneplus_sm6375 -b AOSP-15 kernel/oneplus/sm6375 && \
 git clone https://github.com/anshedu/android_hardware_oplus -b lineage-22.2 hardware/oplus && \
 # Vanilla Build
 . build/envsetup.sh && \
@@ -17,7 +17,7 @@ lunch larry user && make installclean && mka bacon; \
 rm -rf out/target/product/vanilla && rm -rf out/target/product/gapps; \
 cd out/target/product && mv larry vanilla && cd ../../..; \
 # Gapps Build
-cd device/oneplus/larry && rm infinity_larry.mk && mv gapps.txt lineage_larry.mk && cd ../../..; \
+cd device/oneplus/larry && rm infinity_larry.mk && mv gapps.txt infinity_larry.mk && cd ../../..; \
 . build/envsetup.sh; \
 lunch larry user && make installclean && mka bacon; \
 cd out/target/product && mv larry gapps && cd ../../..; \
