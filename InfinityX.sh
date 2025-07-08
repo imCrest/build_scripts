6892b086eaf540ce6335c22bc90f1b45e0c8bda6
Ansh Agnihotri <167695711+anshedu@users.noreply.github.com>
2025-07-08 06:37:03 +0530

    Update InfinityX.sh

--- InfinityX.sh
+++ InfinityX.sh
@@ -5,10 +5,10 @@ rm -rf {device,vendor,kernel,hardware}/oneplus && \
 repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 15 -g default,-mips,-darwin,-notdefault && \
 /opt/crave/resync.sh && \
 git clone https://github.com/anshedu/android_device_oneplus_larry -b Infinity15 device/oneplus/larry && \
-git clone https://github.com/anshedu/android_device_oneplus_sm6375-common -b Infinity15 device/oneplus/sm6375-common && \
-git clone https://github.com/anshedu/proprietary_vendor_oneplus_larry -b AOSP-15 vendor/oneplus/larry && \
-git clone https://github.com/anshedu/proprietary_vendor_oneplus_sm6375-common -b AOSP-15 vendor/oneplus/sm6375-common && \
-git clone https://github.com/anshedu/android_kernel_oneplus_sm6375 -b AOSP-15 kernel/oneplus/sm6375 && \
+git clone https://github.com/anshedu/android_device_oneplus_sm6375-common -b lineage-22.2 device/oneplus/sm6375-common && \
+git clone https://github.com/anshedu/proprietary_vendor_oneplus_larry -b lineage-22.2 vendor/oneplus/larry && \
+git clone https://github.com/anshedu/proprietary_vendor_oneplus_sm6375-common -b lineage-22.2 vendor/oneplus/sm6375-common && \
+git clone https://github.com/anshedu/android_kernel_oneplus_sm6375 -b lineage-22.2 kernel/oneplus/sm6375 && \
 git clone https://github.com/anshedu/android_hardware_oplus -b lineage-22.2 hardware/oplus && \
 # Vanilla Build
 . build/envsetup.sh && \
