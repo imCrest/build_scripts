abccf7e82b0e5ab45b929083c1666972a335624b
Ansh Agnihotri <167695711+anshedu@users.noreply.github.com>
2025-09-10 16:13:07 +0530

    Update EvolutionX.sh

--- EvolutionX.sh
+++ EvolutionX.sh
@@ -5,8 +5,8 @@ rm -rf {device,vendor,kernel,hardware}/oneplus && \
 rm -rf prebuilts/clang/host/linux-x86 && \
 repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b vic --git-lfs -g default,-mips,-darwin,-notdefault && \
 /opt/crave/resync.sh && \
-git clone https://github.com/anshedu/android_device_oneplus_larry -b lineage-23.0 device/oneplus/larry && \
-git clone https://github.com/anshedu/android_device_oneplus_sm6375-common -b lineage-22.2 device/oneplus/sm6375-common && \
+git clone https://github.com/anshedu/android_device_oneplus_larry -b lineage-22.2 device/oneplus/larry && \
+git clone https://github.com/anshedu/android_device_oneplus_sm6375-common -b lineage-23.0 device/oneplus/sm6375-common && \
 git clone https://github.com/anshedu/proprietary_vendor_oneplus_larry -b lineage-23.0 vendor/oneplus/larry && \
 git clone https://github.com/anshedu/proprietary_vendor_oneplus_sm6375-common -b lineage-23.0 vendor/oneplus/sm6375-common && \
 git clone https://github.com/anshedu/android_kernel_oneplus_sm6375 -b lineage-23.0 kernel/oneplus/sm6375 && \
@@ -14,7 +14,6 @@ git clone https://github.com/anshedu/android_hardware_oplus -b lineage-23.0 hard
 # Vanilla Build
 . build/envsetup.sh && \
 lunch lineage_larry-bp2a-user && make installclean && m evolution; \
-rm -rf out/target/product/vanilla && rm -rf out/target/product/gapps; \
 cd out/target/product && mv larry vanilla && cd ../../..; \
 # Gapps Build
 cd device/oneplus/larry && rm larry.mk && mv gapps.txt larry.mk && cd ../../..; \
