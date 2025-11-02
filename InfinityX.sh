8cc811f41aff1832aadff5311e43186bdbcd993f
Ansh Agnihotri <167695711+anshedu@users.noreply.github.com>
2025-11-02 08:30:00 +0530

    Update branch names in InfinityX build script

--- InfinityX.sh
+++ InfinityX.sh
@@ -2,7 +2,7 @@
 
 # =============================
 #   InfinityX Build Script
-#   For: Vanilla → Gapps
+#   For: Vanilla + Gapps
 # =============================
 
 
@@ -16,7 +16,7 @@ repo init -u https://github.com/ProjectInfinity-X/manifest -b 16 --git-lfs && \
 git clone https://github.com/anshedu/android_device_oneplus_larry -b lineage-22.2 device/oneplus/larry && \
 
 # --- Clone Common Device Tree ---
-git clone https://github.com/anshedu/android_device_oneplus_sm6375-common -b lineage-23.0 device/oneplus/sm6375-common && \
+git clone https://github.com/anshedu/android_device_oneplus_sm6375-common -b infinityx16 device/oneplus/sm6375-common && \
 
 # --- Clone Vendor Tree ---
 git clone https://github.com/anshedu/proprietary_vendor_oneplus_larry -b lineage-23.0 vendor/oneplus/larry && \
