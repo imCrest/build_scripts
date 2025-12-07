ea864474327f450da4b4d20979fef021d6813c8a
C R E S T <217463890+imCrest@users.noreply.github.com>
2025-12-06 22:51:59 -0800

    Update Lunaris build script for new repository links

--- Lunaris.sh
+++ Lunaris.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
 # =============================
-#   lunaris Build Script
+#   project lunaris Build Script
 #   For: Vanilla + Gapps
 # =============================
 
 
 # --- Init ROM repo ---
-repo repo init -u https://github.com/Lunaris-AOSP/android -b 16 --git-lfs && \
+repo init -u https://github.com/Lunaris-AOSP/android -b 16 --git-lfs && \
 
 # --- Sync ROM ---
 /opt/crave/resync.sh && \
 
 # --- Clone Device Tree ---
-git clone https://github.com/anshedu/android_device_oneplus_larry -b lineage-22.2 device/oneplus/larry && \
+git clone https://github.com/imCrest/android_device_oneplus_larry -b lunaris device/oneplus/larry && \
 
 # --- Clone Common Device Tree ---
-git clone https://github.com/anshedu/android_device_oneplus_sm6375-common -b lineage-23.0 device/oneplus/sm6375-common && \
+git clone https://github.com/imCrest/android_device_oneplus_sm6375-common1 -b lineage-23.0 device/oneplus/sm6375-common && \
 
 # --- Clone Vendor Tree ---
-git clone https://github.com/anshedu/proprietary_vendor_oneplus_larry -b lineage-23.0 vendor/oneplus/larry && \
+git clone https://github.com/imCrest/proprietary_vendor_oneplus_larry -b lineage-23.0 vendor/oneplus/larry && \
 
 # --- Clone Common Vendor Tree ---
-git clone https://github.com/anshedu/proprietary_vendor_oneplus_sm6375-common -b lineage-23.0 vendor/oneplus/sm6375-common && \
+git clone https://github.com/imCrest/proprietary_vendor_oneplus_sm6375-common -b lineage-23.0 vendor/oneplus/sm6375-common && \
 
 # --- Clone Kernel Tree ---
-git clone https://github.com/anshedu/android_kernel_oneplus_sm6375 -b lineage-23.0 kernel/oneplus/sm6375 && \
+git clone https://github.com/imCrest/android_kernel_oneplus_sm6375 -b lineage-23.0 kernel/oneplus/sm6375 && \
 
 # --- Clone Hardware Tree ---
 git clone https://github.com/LineageOS/android_hardware_oplus -b lineage-23.0 hardware/oplus && \
@@ -37,10 +37,10 @@ git clone https://github.com/LineageOS/android_hardware_oplus -b lineage-23.0 ha
 # --- Vanilla Build ---
 echo "===== Starting Vanilla Build ====="
 . build/envsetup.sh && \
-lunch Lunaris_larry-userdebug && \
+lunch lunaris_larry-userdebug && \
 make installclean && \
 m bacon && \
-mv device/oneplus/larry/Lunaris_larry.mk device/oneplus/larry/vanilla.txt && \
+mv device/oneplus/larry/lunaris_larry.mk device/oneplus/larry/vanilla.txt && \
 
 echo "===== Handling Vanilla Output ====="
 mv out/target/product/larry out/target/product/vanilla && \
