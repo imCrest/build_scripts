3ee66a766a5beda46d811d1085a679a6bf979c6a
C R E S T <217463890+imCrest@users.noreply.github.com>
2026-01-06 13:56:33 +0530

    Update git clone commands for LineageOS branches

--- InfinityX.sh
+++ InfinityX.sh
@@ -16,19 +16,19 @@ repo init -u https://github.com/ProjectInfinity-X/manifest -b 16 --git-lfs && \
 git clone https://github.com/imCrest/android_device_oneplus_larry -b infinityx device/oneplus/larry && \
 
 # --- Clone Common Device Tree ---
-git clone https://github.com/imCrest/android_device_oneplus_sm6375-common1 -b lineage-23.0 device/oneplus/sm6375-common && \
+git clone https://github.com/INXTEN/android_device_oneplus_sm6375-common -b lineage-23.1 device/oneplus/sm6375-common && \
 
 # --- Clone Vendor Tree ---
-git clone https://github.com/imCrest/proprietary_vendor_oneplus_larry -b lineage-23.0 vendor/oneplus/larry && \
+git clone https://github.com/imCrest/proprietary_vendor_oneplus_larry -b lineage-23.1 vendor/oneplus/larry && \
 
 # --- Clone Common Vendor Tree ---
-git clone https://github.com/imCrest/proprietary_vendor_oneplus_sm6375-common -b lineage-23.0 vendor/oneplus/sm6375-common && \
+git clone https://github.com/imCrest/proprietary_vendor_oneplus_sm6375-common -b lineage-23.1 vendor/oneplus/sm6375-common && \
 
 # --- Clone Kernel Tree ---
-git clone https://github.com/imCrest/android_kernel_oneplus_sm6375 -b lineage-23.0 kernel/oneplus/sm6375 && \
+git clone https://github.com/imCrest/android_kernel_oneplus_sm6375 -b lineage-23.1 kernel/oneplus/sm6375 && \
 
 # --- Clone Hardware Tree ---
-git clone https://github.com/LineageOS/android_hardware_oplus -b lineage-23.0 hardware/oplus && \
+git clone https://github.com/LineageOS/android_hardware_oplus -b lineage-23.2 hardware/oplus && \
 
 # =============================
 #  Build: Vanilla → Gapps
