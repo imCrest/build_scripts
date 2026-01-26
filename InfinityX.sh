48003321c0b16edda0f2105e5c7129f23232c997
C R E S T <shrishtiiii96@gmail.com>
2026-01-26 18:22:03 +0530

    Refactor InfinityX build script for clarity

--- InfinityX.sh
+++ InfinityX.sh
@@ -1,52 +1,51 @@
 #!/bin/bash
 
 # =============================
-#   InfinityX Build Script
-#   For: Vanilla + Gapps
+# InfinityX Build Script
+# For: Vanilla + Gapps
 # =============================
-
 
 # --- Init ROM repo ---
 repo init -u https://github.com/ProjectInfinity-X/manifest -b 16 --git-lfs && \
-
 # --- Sync ROM ---
 /opt/crave/resync.sh && \
-
 # --- Clone Device Tree ---
 git clone https://github.com/imCrest/android_device_oneplus_larry -b infinityx device/oneplus/larry && \
-
 # --- Clone Common Device Tree ---
 git clone https://github.com/imCrest/android_device_oneplus_sm6375-common -b lineage-23.1 device/oneplus/sm6375-common && \
-
 # --- Clone Vendor Tree ---
 git clone https://github.com/imCrest/proprietary_vendor_oneplus_larry -b lineage-23.1 vendor/oneplus/larry && \
-
 # --- Clone Common Vendor Tree ---
 git clone https://github.com/imCrest/proprietary_vendor_oneplus_sm6375-common -b lineage-23.1 vendor/oneplus/sm6375-common && \
-
 # --- Clone Kernel Tree ---
 git clone https://github.com/imCrest/android_kernel_oneplus_sm6375 -b lineage-23.1 kernel/oneplus/sm6375 && \
-
 # --- Clone Hardware Tree ---
 git clone https://github.com/imCrest/android_hardware_oplus -b lineage-23.1 hardware/oplus && \
 
+# =============================
+# Build: Gapps → Vanilla
+# =============================
 
-export WITH_GMS=true
-export TARGET_SUPPORTS_GAPPS=true
-export TARGET_SUPPORTS_GSUITE=true
+# --- Gapps Build ---
+echo "===== Starting Gapps Build =====" && \
+export WITH_GMS=true && \
+export TARGET_SUPPORTS_GAPPS=true && \
+export TARGET_SUPPORTS_GSUITE=true && \
+rm -rf out/soong out/target/product/larry && \
+source build/envsetup.sh && \
+lunch infinity_larry-userdebug && \
+mka bacon -j$(nproc) && \
+mv out/target/product/larry out/target/product/gapps && \
 
-rm -rf out/soong out/target/product/larry
-source build/envsetup.sh
-lunch infinity_larry-userdebug
-mka bacon -j$(nproc)
-mv out/target/product/larry out/target/product/gapps
+# --- Vanilla Build ---
+echo "===== Starting Vanilla Build =====" && \
+export WITH_GMS=false && \
+export TARGET_SUPPORTS_GAPPS=false && \
+export TARGET_SUPPORTS_GSUITE=false && \
+rm -rf out/soong out/target/product/larry && \
+source build/envsetup.sh && \
+lunch infinity_larry-userdebug && \
+mka bacon -j$(nproc) && \
+mv out/target/product/larry out/target/product/vanilla && \
 
-export WITH_GMS=false
-export TARGET_SUPPORTS_GAPPS=false
-export TARGET_SUPPORTS_GSUITE=false
-
-rm -rf out/soong out/target/product/larry
-source build/envsetup.sh
-lunch infinity_larry-userdebug
-mka bacon -j$(nproc)
-mv out/target/product/larry out/target/product/vanilla
+echo "===== All builds completed successfully! ====="
