b3d3a02464d792c2462e31dc166d7938a93c443d
C R E S T <shrishtiiii96@gmail.com>
2026-01-27 11:45:08 +0530

    Update InfinityX.sh

--- InfinityX.sh
+++ InfinityX.sh
@@ -2,25 +2,25 @@
 
 # =============================
 # InfinityX Build Script
-# For: Vanilla + Gapps
+# Fast version for Crave.io
 # =============================
 
 # --- Init ROM repo ---
-repo init -u https://github.com/ProjectInfinity-X/manifest -b 16 --git-lfs && \
+repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16-QPR2 -g default,-mips,-darwin,-notdefault && \
 # --- Sync ROM ---
 /opt/crave/resync.sh && \
 # --- Clone Device Tree ---
 git clone https://github.com/imCrest/android_device_oneplus_larry -b infinityx device/oneplus/larry && \
 # --- Clone Common Device Tree ---
-git clone https://github.com/imCrest/android_device_oneplus_sm6375-common -b lineage-23.1 device/oneplus/sm6375-common && \
+git clone https://github.com/imCrest/android_device_oneplus_sm6375-common -b lineage-23.2 device/oneplus/sm6375-common && \
 # --- Clone Vendor Tree ---
-git clone https://github.com/imCrest/proprietary_vendor_oneplus_larry -b lineage-23.1 vendor/oneplus/larry && \
+git clone https://github.com/imCrest/proprietary_vendor_oneplus_larry -b lineage-23.2 vendor/oneplus/larry && \
 # --- Clone Common Vendor Tree ---
-git clone https://github.com/imCrest/proprietary_vendor_oneplus_sm6375-common -b lineage-23.1 vendor/oneplus/sm6375-common && \
+git clone https://github.com/imCrest/proprietary_vendor_oneplus_sm6375-common -b lineage-23.2 vendor/oneplus/sm6375-common && \
 # --- Clone Kernel Tree ---
-git clone https://github.com/imCrest/android_kernel_oneplus_sm6375 -b lineage-23.1 kernel/oneplus/sm6375 && \
+git clone https://github.com/imCrest/android_kernel_oneplus_sm6375 -b lineage-23.2 kernel/oneplus/sm6375 && \
 # --- Clone Hardware Tree ---
-git clone https://github.com/imCrest/android_hardware_oplus -b lineage-23.1 hardware/oplus && \
+git clone https://github.com/imCrest/android_hardware_oplus -b lineage-23.2 hardware/oplus && \
 
 # =============================
 # Build: Gapps → Vanilla
@@ -31,9 +31,9 @@ echo "===== Starting Gapps Build =====" && \
 export WITH_GMS=true && \
 export TARGET_SUPPORTS_GAPPS=true && \
 export TARGET_SUPPORTS_GSUITE=true && \
-rm -rf out/soong out/target/product/larry && \
 source build/envsetup.sh && \
 lunch infinity_larry-userdebug && \
+make installclean && \
 mka bacon -j$(nproc) && \
 mv out/target/product/larry out/target/product/gapps && \
 
@@ -42,9 +42,9 @@ echo "===== Starting Vanilla Build =====" && \
 export WITH_GMS=false && \
 export TARGET_SUPPORTS_GAPPS=false && \
 export TARGET_SUPPORTS_GSUITE=false && \
-rm -rf out/soong out/target/product/larry && \
 source build/envsetup.sh && \
 lunch infinity_larry-userdebug && \
+make installclean && \
 mka bacon -j$(nproc) && \
 mv out/target/product/larry out/target/product/vanilla && \
 
