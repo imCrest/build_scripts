dc65da0be6b8fb2d776ae7db670c7c21881119bd
C R E S T <shrishtiiii96@gmail.com>
2026-02-08 13:49:04 +0530

    Optimize git clone and ccache setup in script
    Refactor git clone commands to check for existing directories before cloning. Adjust ccache configuration and manage output files for GApps and vanilla builds.

--- aws-infinity.sh
+++ aws-infinity.sh
@@ -7,10 +7,7 @@ export GIT_TERMINAL_PROMPT=0
 
 sudo swapoff -a || true
 sudo rm -f /swapfile || true
-
-export USE_CCACHE=1
-export CCACHE_DIR=~/.ccache
-ccache -M 80G
+sudo sed -i '/swapfile/d' /etc/fstab || true
 
 sudo apt update
 sudo apt install -y git git-lfs curl zip unzip bc bison build-essential \
@@ -20,6 +17,12 @@ libxml2-utils lzop openjdk-17-jdk python-is-python3 rsync schedtool \
 squashfs-tools xsltproc zlib1g-dev tmux rclone
 
 git lfs install
+
+export USE_CCACHE=1
+export CCACHE_EXEC=/usr/bin/ccache
+export CCACHE_DIR=$HOME/.ccache
+ccache -M 80G
+ccache -z
 
 mkdir -p ~/bin
 curl -s https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
@@ -33,12 +36,15 @@ cd infinityx
 yes | repo init --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16
 repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync -j24
 
-git clone https://github.com/imCrest/android_device_oneplus_larry -b infinityx device/oneplus/larry || true
-git clone https://github.com/imCrest/android_device_oneplus_sm6375-common -b lineage-23.2 device/oneplus/sm6375-common || true
-git clone https://github.com/imCrest/proprietary_vendor_oneplus_larry -b lineage-23.2 vendor/oneplus/larry || true
-git clone https://github.com/imCrest/proprietary_vendor_oneplus_sm6375-common -b lineage-23.2 vendor/oneplus/sm6375-common || true
-git clone https://github.com/imCrest/android_kernel_oneplus_sm6375 -b lineage-23.2 kernel/oneplus/sm6375 || true
-git clone https://github.com/imCrest/android_hardware_oplus -b lineage-23.2 hardware/oplus || true
+[ -d device/oneplus/larry ] || git clone https://github.com/imCrest/android_device_oneplus_larry -b infinityx device/oneplus/larry
+[ -d device/oneplus/sm6375-common ] || git clone https://github.com/imCrest/android_device_oneplus_sm6375-common -b lineage-23.2 device/oneplus/sm6375-common
+[ -d vendor/oneplus/larry ] || git clone https://github.com/imCrest/proprietary_vendor_oneplus_larry -b lineage-23.2 vendor/oneplus/larry
+[ -d vendor/oneplus/sm6375-common ] || git clone https://github.com/imCrest/proprietary_vendor_oneplus_sm6375-common -b lineage-23.2 vendor/oneplus/sm6375-common
+[ -d kernel/oneplus/sm6375 ] || git clone https://github.com/imCrest/android_kernel_oneplus_sm6375 -b lineage-23.2 kernel/oneplus/sm6375
+[ -d hardware/oplus ] || git clone https://github.com/imCrest/android_hardware_oplus -b lineage-23.2 hardware/oplus
+
+export SOONG_UI_THREADS=16
+export NINJA_ARGS="-j24"
 
 source build/envsetup.sh
 lunch infinity_larry-userdebug
@@ -47,17 +53,20 @@ export WITH_GMS=false
 export TARGET_SUPPORTS_GAPPS=false
 export TARGET_SUPPORTS_GSUITE=false
 
-make installclean
+mv device/oneplus/larry/infinity_larry.mk device/oneplus/larry/vanilla.txt
 mka bacon -j24
 mv out/target/product/larry out/target/product/vanilla
+
+mv device/oneplus/larry/gapps.txt device/oneplus/larry/infinity_larry.mk
 
 export WITH_GMS=true
 export TARGET_SUPPORTS_GAPPS=true
 export TARGET_SUPPORTS_GSUITE=true
 
-make installclean
 mka bacon -j24
 mv out/target/product/larry out/target/product/gapps
+
+mv device/oneplus/larry/vanilla.txt device/oneplus/larry/infinity_larry.mk
 
 PIXELDRAIN_KEY="f869dbb7-758e-4efa-9440-e1418b1c9916"
 
@@ -66,3 +75,5 @@ curl -T *.zip -u :$PIXELDRAIN_KEY https://pixeldrain.com/api/file/
 
 cd ../vanilla
 curl -T *.zip -u :$PIXELDRAIN_KEY https://pixeldrain.com/api/file/
+
+ccache -s
