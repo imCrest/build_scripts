f97c8adcf69ae731bc8ea173664f0b187b2acaf9
C R E S T <shrishtiiii96@gmail.com>
2026-03-06 18:33:07 +0530

    Refactor file uploads and Telegram message sending
    Refactor upload process for files and send Telegram notification with project details.

--- InfinityX.sh
+++ InfinityX.sh
@@ -28,12 +28,71 @@ mka bacon -j$(nproc) && \
 mv out/target/product/larry out/target/product/vanilla
 
 PIXELDRAIN_KEY="f869dbb7-758e-4efa-9440-e1418b1c9916"
+TELEGRAM_TOKEN="8172049270:AAGg1I0ah8CNV0PwtNg9cTz6AidYQLR4WQw"
+CHAT_ID="7911062735"
 
-cd out/target/product/gapps
-curl -T *.zip -u :$PIXELDRAIN_KEY https://pixeldrain.com/api/file/
-curl -T boot.img -u :$PIXELDRAIN_KEY https://pixeldrain.com/api/file/
-curl -T vendor_boot.img -u :$PIXELDRAIN_KEY https://pixeldrain.com/api/file/
-curl -T dtbo.img -u :$PIXELDRAIN_KEY https://pixeldrain.com/api/file/
+upload_and_get_link () {
+    FILE=$1
+    if [ ! -f "$FILE" ]; then
+        echo "#"
+        return 1
+    fi
+    RESPONSE=$(curl -s -T "$FILE" -u :$PIXELDRAIN_KEY https://pixeldrain.com/api/file/)
+    ID=$(echo "$RESPONSE" | sed -n 's/.*"id":"\([^"]*\)".*/\1/p')
+    if [ -n "$ID" ]; then
+        echo "https://pixeldrain.com/u/$ID"
+    else
+        echo "#"
+    fi
+}
 
-cd ../vanilla
-curl -T *.zip -u :$PIXELDRAIN_KEY https://pixeldrain.com/api/file/
+if cd out/target/product/gapps; then
+    ROM_GAPPS=$(upload_and_get_link *.zip)
+    BOOT=$(upload_and_get_link boot.img)
+    VENDOR_BOOT=$(upload_and_get_link vendor_boot.img)
+    DTBO=$(upload_and_get_link dtbo.img)
+    cd ../../../.. 
+else
+    exit 1
+fi
+
+if cd out/target/product/vanilla; then
+    ROM_VANILLA=$(upload_and_get_link *.zip)
+    cd ../../../.. 
+else
+    exit 1
+fi
+
+DATE=$(date +'%d/%B/%Y')
+
+MESSAGE="<b>Project Infinity X (Unofficial) | Android 16 (QPR-2)</b>
+<b>Updated: $DATE</b>
+
+<b>Maintainer</b> ~ <a href=\"tg://openmessage?user_id=7911062735\">SUJΛL</a>
+
+<tg-spoiler><a href=\"https://t.me/OnePlusNordCE3Lite/125856\">Before Flashing Any Rom Read This Note First</a></tg-spoiler>
+
+Gapps > <a href=\"$ROM_GAPPS\">DOWNLOAD</a>
+
+Vanilla > <a href=\"$ROM_VANILLA\">DOWNLOAD</a>
+
+3.8V Changelogs - <a href=\"YOUR_CHANGELOG_LINK_HERE\">HERE</a>
+Rom Screenshot - <a href=\"https://t.me/ProjectInfinityX/1697?single\">HERE</a>
+Flashing Steps - <a href=\"https://youtu.be/vs2y1MAVWO0?si=FLFk-Igi1Be01SVo\">HERE</a>
+
+Notes:
+<blockquote>1. Dirty Flash will be fine if you using 3.6 or 3.7 Build</blockquote>
+
+<blockquote>2. Here (Gapps)  <a href=\"$BOOT\">Boot.img</a>  <a href=\"$VENDOR_BOOT\">Vendor_boot.img</a>  <a href=\"$DTBO\">dtbo.img</a></blockquote>
+
+<blockquote>3. If you have any query related to the device then please ask in the community, Please Don't DM Me</blockquote>
+
+If you want more updates in the future nd <a href=\"https://t.me/Sujxlsahu?text=I%20want%20to%20donate%20to%20you\">Donate</a> only if you feel I deserve it (Optional)
+
+Enjoy.."
+
+curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendMessage" \
+-d chat_id="$CHAT_ID" \
+-d parse_mode="HTML" \
+-d disable_web_page_preview="true" \
+-d text="$MESSAGE" > /dev/null
