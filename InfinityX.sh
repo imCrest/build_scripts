dea0f82a711daaef75b65e4971cf1f81816f8e31
C R E S T <shrishtiiii96@gmail.com>
2026-03-06 15:15:06 +0530

    Add additional curl commands for file uploads

--- InfinityX.sh
+++ InfinityX.sh
@@ -26,10 +26,14 @@ lunch infinity_larry-userdebug && \
 make installclean && \
 mka bacon -j$(nproc) && \
 mv out/target/product/larry out/target/product/vanilla
+
 PIXELDRAIN_KEY="f869dbb7-758e-4efa-9440-e1418b1c9916"
 
 cd out/target/product/gapps
 curl -T *.zip -u :$PIXELDRAIN_KEY https://pixeldrain.com/api/file/
+curl -T boot.img -u :$PIXELDRAIN_KEY https://pixeldrain.com/api/file/
+curl -T vendor_boot.img -u :$PIXELDRAIN_KEY https://pixeldrain.com/api/file/
+curl -T dtbo.img -u :$PIXELDRAIN_KEY https://pixeldrain.com/api/file/
 
 cd ../vanilla
 curl -T *.zip -u :$PIXELDRAIN_KEY https://pixeldrain.com/api/file/
