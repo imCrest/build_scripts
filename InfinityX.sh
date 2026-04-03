c2b4300fd9a6c1a62dfdb904001666bc434a58bc
C R E S T <shrishtiiii96@gmail.com>
2026-04-03 10:23:25 +0530

    Clean up InfinityX.sh script formatting
    Refactor script to remove unnecessary line breaks and improve readability.

--- InfinityX.sh
+++ InfinityX.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+rm -rf .repo
+rm -rf out
+rm -rf device/oneplus/larry
+rm -rf device/oneplus/sm6375-common
+rm -rf vendor/oneplus/larry
+rm -rf vendor/oneplus/sm6375-common
+rm -rf kernel/oneplus/sm6375
+rm -rf hardware/oplus
+
 repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault && \
 /opt/crave/resync.sh && \
 git clone https://github.com/imCrest/android_device_oneplus_larry -b infinityx device/oneplus/larry && \
@@ -8,16 +18,14 @@ git clone https://github.com/imCrest/proprietary_vendor_oneplus_sm6375-common -b
 git clone https://github.com/imCrest/android_kernel_oneplus_sm6375 -b lineage-23.2 kernel/oneplus/sm6375 && \
 git clone https://github.com/imCrest/android_hardware_oplus -b lineage-23.2 hardware/oplus && \
 
-export WITH_GMS=true && \
-export TARGET_SUPPORTS_GAPPS=true && \
-export TARGET_SUPPORTS_GSUITE=true && \
-source build/envsetup.sh && \
-lunch infinity_larry-userdebug && \
-make installclean && \
-mka bacon -j$(nproc) && \
-mv out/target/product/larry out/target/product/gapps
-
-PIXELDRAIN_KEY="f869dbb7-758e-4efa-9440-e1418b1c9916"
+export WITH_GMS=true
+export TARGET_SUPPORTS_GAPPS=true
+export TARGET_SUPPORTS_GSUITE=true
+source build/envsetup.sh
+lunch infinity_larry-userdebug
+make installclean
+mka bacon -j$(nproc)
+mv out/target/product/larry out/target/product/gappsPIXELDRAIN_KEY="f869dbb7-758e-4efa-9440-e1418b1c9916"
 TELEGRAM_TOKEN="8172049270:AAGg1I0ah8CNV0PwtNg9cTz6AidYQLR4WQw"
 CHAT_ID="7911062735"
 
