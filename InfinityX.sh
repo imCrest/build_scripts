e274ec3f62ffcb7b53a69052d3b6c483f457e898
C R E S T <217463890+imCrest@users.noreply.github.com>
2026-09-12 18:47:37 +0530

    Update InfinityX.sh

--- InfinityX.sh
+++ InfinityX.sh
@@ -6,12 +6,12 @@ rm -rf .repo/local_manifests out/target/product/larry device/oneplus/larry devic
 
 repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault && \
 /opt/crave/resync.sh && \
-git clone https://github.com/imCrest/android_device_oneplus_larry -b infinityx device/oneplus/larry && \
-git clone https://github.com/imCrest/android_device_oneplus_sm6375-common -b lineage-23.2 device/oneplus/sm6375-common && \
-git clone https://gitlab.com/ViaanLarryROMS/proprietary_vendor_oneplus_larry -b 23.2-no-firmware vendor/oneplus/larry && \
+git clone https://github.com/LineageOS/android_device_oneplus_larry -b lineage-24.0 device/oneplus/larry && \
+git clone https://github.com/LineageOS/android_device_oneplus_sm6375-common -b lineage-24.0 device/oneplus/sm6375-common && \
+git clone https://github.com/TheMuppets/proprietary_vendor_oneplus_larry -b lineage-23.2 vendor/oneplus/larry && \
 git clone https://github.com/TheMuppets/proprietary_vendor_oneplus_sm6375-common -b lineage-23.2 vendor/oneplus/sm6375-common && \
-git clone https://github.com/imCrest/android_kernel_oneplus_sm6375 -b lineage-23.2 kernel/oneplus/sm6375 && \
-git clone https://github.com/imCrest/android_hardware_oplus -b lineage-23.2 hardware/oplus && \
+git clone https://github.com/LineageOS/android_kernel_oneplus_sm6375 -b lineage-24.0 kernel/oneplus/sm6375 && \
+git clone https://github.com/LineageOS/android_hardware_oplus -b lineage-24.0 hardware/oplus && \
 
 export WITH_GMS=true && export TARGET_SUPPORTS_GAPPS=true && export TARGET_SUPPORTS_GSUITE=true && \
 source build/envsetup.sh && lunch infinity_larry-userdebug && make installclean && mka bacon -j$(nproc) && \
