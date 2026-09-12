ae70109eeda7bb33dc8b777874af5ce1e0e9c86b
C R E S T <217463890+imCrest@users.noreply.github.com>
2026-09-12 19:34:20 +0530

    Update InfinityX.sh

--- InfinityX.sh
+++ InfinityX.sh
@@ -6,7 +6,7 @@ rm -rf .repo/local_manifests out/target/product/larry device/oneplus/larry devic
 
 repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault && \
 /opt/crave/resync.sh && \
-git clone https://github.com/LineageOS/android_device_oneplus_larry -b lineage-24.0 device/oneplus/larry && \
+git clone https://github.com/LineageOS/android_device_oneplus-larry -b lineage-24.0 device/oneplus/larry && \
 git clone https://github.com/LineageOS/android_device_oneplus_sm6375-common -b lineage-24.0 device/oneplus/sm6375-common && \
 git clone https://github.com/TheMuppets/proprietary_vendor_oneplus_larry -b lineage-23.2 vendor/oneplus/larry && \
 git clone https://github.com/TheMuppets/proprietary_vendor_oneplus_sm6375-common -b lineage-23.2 vendor/oneplus/sm6375-common && \
