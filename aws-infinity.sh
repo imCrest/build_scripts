b942f48fd93f37e91ead605f9fd7f3e000f8c453
C R E S T <217463890+imCrest@users.noreply.github.com>
2026-09-20 15:34:00 +0530

    Update aws-infinity.sh

--- aws-infinity.sh
+++ aws-infinity.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-sudo apt-get update && sudo apt-get install gh -y
-
-rm -rf .repo/local_manifests out/target/product/larry device/oneplus/larry device/oneplus/sm6375-common vendor/oneplus/larry vendor/oneplus/sm6375-common kernel/oneplus/sm6375 hardware/oplus && \
-
 yes | repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 17 -g default,-mips,-darwin,-notdefault && \
 
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) && \
