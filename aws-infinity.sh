11637a07ea1e4eec7595e675412106ab64f64893
C R E S T <217463890+imCrest@users.noreply.github.com>
2026-09-24 10:53:54 +0530

    Update aws-infinity.sh

--- aws-infinity.sh
+++ aws-infinity.sh
@@ -7,8 +7,8 @@ repo sync -j1 --fail-fast && \
 
 git clone https://github.com/imCrest/android_device_oneplus-larry -b lineage-24.0 device/oneplus/larry && \
 git clone https://github.com/LineageOS/android_device_oneplus_sm6375-common -b lineage-24.0 device/oneplus/sm6375-common && \
-git clone https://github.com/TheMuppets/proprietary_vendor_oneplus_larry -b lineage-23.2 vendor/oneplus/larry && \
-git clone https://github.com/TheMuppets/proprietary_vendor_oneplus_sm6375-common -b lineage-23.2 vendor/oneplus/sm6375-common && \
+git clone https://github.com/TheMuppets/proprietary_vendor_oneplus_larry -b lineage-24 vendor/oneplus/larry && \
+git clone https://github.com/TheMuppets/proprietary_vendor_oneplus_sm6375-common -b lineage-24 vendor/oneplus/sm6375-common && \
 git clone https://github.com/LineageOS/android_kernel_oneplus_sm6375 -b lineage-24.0 kernel/oneplus/sm6375 && \
 git clone https://github.com/LineageOS/android_hardware_oplus -b lineage-24.0 hardware/oplus && \
 
